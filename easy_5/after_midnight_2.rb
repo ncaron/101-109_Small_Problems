a3aaf52b6c560864b8b2106b6ab15e1a7696fbfe
Niko Caron <niko.caron90@gmail.com>
2017-02-28 07:16:29 -0800

    Added Easy 5 questions.

---
+++ easy_5/after_midnight_2.rb
@@ -0,0 +1,17 @@
+# As seen in the previous exercise, the time of day can be represented as the number of minutes before or after midnight.
+# If the number of minutes is positive, the time is after midnight.
+# If the number of minutes is negative, the time is before midnight.
+
+# Write two methods that each take a time of day in 24 hour format, and return the number of minutes before and after midnight,
+# respectively. Both methods should return a value in the range 0..1439.
+
+# You may not use ruby's Date and Time methods.
+
+# Disregard Daylight Savings and Standard Time and other irregularities.
+
+puts after_midnight('00:00') == 0
+puts before_midnight('00:00') == 0
+puts after_midnight('12:34') == 754
+puts before_midnight('12:34') == 686
+puts after_midnight('24:00') == 0
+puts before_midnight('24:00') == 0
